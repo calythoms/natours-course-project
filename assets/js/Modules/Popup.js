85207b3c11f71f4382422ba14a81e44a18b60c1e
calythoms <carolinethomas@gmx.fr>
2023-03-02 17:38:40 +0100

    Finished JS logic to close popup

--- assets/js/Modules/Popup.js
+++ assets/js/Modules/Popup.js
@@ -1,17 +1,24 @@
 class Popup {
-    // Click outsite popup closes it DONE
-    // Esc key closes it
     constructor() {
         this.popupBackground = document.querySelector('.popup');
         this.events();
     }
 
     events() {
-        this.popupBackground.addEventListener('click', (e) => this.outsidePopupClickClose(e))
+        this.popupBackground.addEventListener('click', (e) => this.outsidePopupClickClose(e));
+        window.addEventListener('keydown', (e) => this.escClosePopup(e));
     }
 
     outsidePopupClickClose(e) {
         if (e.target.closest('.popup__content')) return;
+        this.closePopup();
+    }
+
+    escClosePopup(e) {
+        if (e.code === 'Escape' && window.location.href.includes('#popup')) this.closePopup();
+    }
+
+    closePopup() {
         window.location.href = '#section-tours';
     }
 }
